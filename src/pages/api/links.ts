1788237e972353103a850fce4a0ca8e1a82f1fb0
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-07-04 20:29:01 +0200

    ✨ api: Add links route

---
+++ src/pages/api/links.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../server/db/client';
+import Randomstring from 'randomstring';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST')
+    return res.status(405).json({ error: 'Only POST is allowed' });
+
+  const result = await prisma.link.create({
+    data: {
+      ...req.body,
+      slug: Randomstring.generate(8),
+    },
+  });
+
+  res.status(200).json(result);
+};
