7469d147dd118c602a29079d928e4bcd0d621cd6
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-07-05 14:37:46 +0200

    ✨ api: Add routes for incrementing counters

--- src/pages/api/links/[slug]/completed-count.ts
+++ src/pages/api/links/[slug]/completed-count.ts
@@ -1,13 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../../server/db/client';
+import { prisma } from '../../../../server/db/client';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'PUT')
+    return res.status(405).json({ error: 'Only PUT is allowed' });
+
   const { slug } = req.query;
   if (typeof slug !== 'string')
     return res.status(400).json({ error: 'Slug should be a string' });
 
-  const result = await prisma.link.findFirst({
+  const result = await prisma.link.update({
     where: { slug },
+    data: { completedCount: { increment: 1 } },
   });
 
   res.status(200).json(JSON.parse(JSON.stringify(result)));
---
+++ src/pages/api/links/[slug]/visited-count.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../../server/db/client';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'PUT')
+    return res.status(405).json({ error: 'Only PUT is allowed' });
+
+  const { slug } = req.query;
+  if (typeof slug !== 'string')
+    return res.status(400).json({ error: 'Slug should be a string' });
+
+  const result = await prisma.link.update({
+    where: { slug },
+    data: { visitedCount: { increment: 1 } },
+  });
+
+  res.status(200).json(JSON.parse(JSON.stringify(result)));
+};
