4660f9450bf92fc9d4a40333ba50641311499e59
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-07-11 21:40:22 +0200

    🔧 tailwind.config.js: Add glow box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'glow-blue': '0 0 16px 0 rgba(59, 130, 246, 0.3)',
+        'glow-red': '0 0 16px 0 rgba(239, 68, 68, 0.3)',
+      },
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
     },
