103dde7760f98d1f1b2a1f2e9a92818311093cca
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-07-16 16:50:12 +0200

    ✨ /api/links/[slug]: Implement delete

--- src/pages/api/links/[slug]/index.ts
+++ src/pages/api/links/[slug]/index.ts
@@ -1,14 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../../../server/db/client';
+import { unstable_getServerSession as getServerSession } from 'next-auth/next';
+import { authOptions } from '../../auth/[...nextauth]';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
   if (typeof slug !== 'string')
     return res.status(400).json({ error: 'Slug should be a string' });
 
-  const result = await prisma.link.findFirst({
-    where: { slug },
-  });
+  if (req.method === 'GET') {
+    const result = await prisma.link.findFirst({
+      where: { slug },
+    });
+    return res.status(200).json(JSON.parse(JSON.stringify(result)));
+  }
 
-  res.status(200).json(JSON.parse(JSON.stringify(result)));
+  if (req.method === 'DELETE') {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) return res.status(401).json({ error: 'Not authenticated' });
+
+    const linkToDelete = await prisma.link.findFirst({
+      where: { slug },
+    });
+    if (session.user?.email !== linkToDelete?.userEmail)
+      return res.status(401).json({ error: 'Not authenticated' });
+
+    const deletedLink = await prisma.link.delete({
+      where: { slug },
+    });
+
+    return res.status(200).json(JSON.parse(JSON.stringify(deletedLink)));
+  }
+
+  res.status(405).json({ error: 'Method not allowed' });
 };
