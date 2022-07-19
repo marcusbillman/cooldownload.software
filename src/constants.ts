c70060fdde980d066957eed909e9656ce9d66834
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-07-19 19:51:45 +0200

    🍱 constants.ts: Add desc and GIF to challenges

--- src/constants.ts
+++ src/constants.ts
@@ -2,18 +2,29 @@ export const CHALLENGES = [
   {
     name: 'wait',
     friendlyName: 'Wait',
+    description:
+      'Simply wait for 10 seconds, except the timer randomly freezes and rewinds.',
+    demoUrl: '/assets/challenge-demos/wait.gif',
   },
   {
     name: '3d-text',
     friendlyName: '3D Text',
+    description: 'Type the text that is frantically spinning around.',
+    demoUrl: '/assets/challenge-demos/3d-text.gif',
   },
   {
     name: 'rotate-image',
     friendlyName: 'Rotate Image',
+    description:
+      'Rotate the image until it looks upright, using the borderline unusable slider.',
+    demoUrl: '/assets/challenge-demos/rotate-image.gif',
   },
   {
     name: 'select-squares',
     friendlyName: 'Select Squares',
+    description:
+      'That darn "select all squares with traffic lights" thing just got way worse.',
+    demoUrl: '/assets/challenge-demos/select-squares.gif',
   },
 ];
 
